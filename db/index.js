7f1819532a318d98bb484ccba0e5413f36be0dc5
Jay <JJ.Buckby@hotmail.co.uk>
2023-03-02 16:26:21 +0000

    Updated files for docker integration

--- db/index.js
+++ db/index.js
@@ -4,11 +4,12 @@ import { config } from "dotenv";
 config();
 
 export const sequelize = new Sequelize(
-  "todo_list",
+  process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PW,
   {
     host: process.env.DB_HOST,
     dialect: "mysql",
+    port: process.env.PORT,
   }
 );
