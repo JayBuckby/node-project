a79a2549e5144e6b3c08b4558c3ed10841bda075
Jay <JJ.Buckby@hotmail.co.uk>
2023-03-06 16:16:45 +0000

    Made changes to connnect DB and API

--- db/index.js
+++ db/index.js
@@ -3,13 +3,7 @@ import { config } from "dotenv";
 
 config();
 
-export const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PW,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    port: process.env.PORT,
-  }
-);
+export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  host: process.env.DB_HOST,
+  dialect: "mysql",
+});
